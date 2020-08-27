c58d490b5db1548c76bbf5c05131eb68ac60f7e0
Eduardo Marques Balbo <eduardo-balbo@hotmail.com>
2020-08-27 17:16:36 -0300

    test: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -127,4 +127,10 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('should call TokenGenerator with correct id', async () => {
+    const { sut } = makeSut()
+    const acessToken = await sut.auth(makeFakeAuthentication())
+    expect(acessToken).toBe('any_token')
+  })
 })
