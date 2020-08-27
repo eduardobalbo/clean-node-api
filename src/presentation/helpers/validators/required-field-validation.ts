f43dfb44781cf052fde8d307c73bda205d0a69f7
Eduardo Marques Balbo <eduardo-balbo@hotmail.com>
2020-08-27 07:52:06 -0300

    refactor: move Validation to protocols folder

--- src/presentation/helpers/validators/required-field-validation.ts
+++ src/presentation/helpers/validators/required-field-validation.ts
@@ -1,4 +1,4 @@
-import { Validation } from './validation'
+import { Validation } from '../../protocols/validation'
 import { MissingParamError } from '../../errors'
 
 // recebe input especifico e valida se existe no corpo da requisicao
