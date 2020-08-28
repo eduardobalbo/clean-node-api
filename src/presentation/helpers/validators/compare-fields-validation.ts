013867f3dc8ae57c7d5db8b967ec10b6f17cf26f
Eduardo Marques Balbo <eduardo-balbo@hotmail.com>
2020-08-28 16:14:13 -0300

    refactor: remove duplicated code

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -4,13 +4,10 @@ import { InvalidParamError } from '../../errors'
 // recebe input especifico e valida se existe no corpo da requisicao
 
 export class CompareFieldsValidation implements Validation {
-  private readonly fieldName: string
-  private readonly fieldToCompareName: string
-
-  constructor (fieldName: string, fieldToCompareName: string) {
-    this.fieldName = fieldName
-    this.fieldToCompareName = fieldToCompareName
-  }
+  constructor (
+    private readonly fieldName: string,
+    private readonly fieldToCompareName: string
+  ) {}
 
   validate (input: any): Error {
     if (input[this.fieldName] !== input[this.fieldToCompareName]) {
--- src/presentation/helpers/validators/email-validation.ts
+++ src/presentation/helpers/validators/email-validation.ts
@@ -5,13 +5,10 @@ import { EmailValidator } from '../../protocols/email-validator'
 // recebe input especifico e valida se existe no corpo da requisicao
 
 export class EmailValidation implements Validation {
-  private readonly fieldName: string
-  private readonly emailValidator: EmailValidator
-
-  constructor (fieldName: string, emailValidator: EmailValidator) {
-    this.emailValidator = emailValidator
-    this.fieldName = fieldName
-  }
+  constructor (
+    private readonly fieldName: string,
+    private readonly emailValidator: EmailValidator
+  ) {}
 
   validate (input: any): Error {
     const isValid = this.emailValidator.isValid(input[this.fieldName])
